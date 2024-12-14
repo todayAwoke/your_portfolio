de8c059be0fdcfc2b7487727a2cc83e0bf1eb48c
awokedejenie@gmail.com <todayAwoke>
2024-12-14 11:48:45 +0300

    add blogs with google sheet

--- components/home/Blog/Blog.tsx
+++ components/home/Blog/Blog.tsx
@@ -1,10 +1,21 @@
-import { blogs } from '@/app/Data/data'
-import SectionHeading from '@/components/helper/SectionHeading'
-import React ,{useEffect,useState} from 'react'
-import BlogCard from './BlogCard'
+import { useEffect, useState } from 'react';
+import SectionHeading from '@/components/helper/SectionHeading';
+import BlogCard from './BlogCard';
+
+// Define the type for blog post
+interface BlogPost {
+    title: string;
+    summary: string;
+    detail: string;
+    date: string;
+    image: string;
+}
 
 const Blog = () => {
-    const [posts, setPosts] = useState([]);
+    const [posts, setPosts] = useState<BlogPost[]>([]);
+    const [loading, setLoading] = useState<boolean>(true);
+    const [error, setError] = useState<string | null>(null);
+
     useEffect(() => {
         const fetchPosts = async () => {
             try {
@@ -14,29 +25,37 @@ const Blog = () => {
                 }
                 const data = await response.json();
                 setPosts(data.data);
-            } catch (err:any) {
-                console.log((err.message));
-            } 
+            } catch (err: any) {
+                setError(err.message);
+            } finally {
+                setLoading(false);
+            }
         };
 
         fetchPosts();
     }, []);
 
+    if (loading) {
+        return <div className='text-white text-center'>Loading...</div>;
+    }
+
+    if (error) {
+        return <div className='text-red-500 text-center'>Error: {error}</div>;
+    }
+
     return (
-        <div className='pt-28 pb-16 bg-gray-800' id='blogs' >
+        <div className='pt-28 pb-16 bg-gray-800' id='blogs'>
             <SectionHeading>My Blogs</SectionHeading>
             <div className='w-[80%] mx-auto mt-20 grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-12 items-center'>
-{posts.map((post,index)=>{
-    return <div key={index}>
-        {/* blog card */}
-        <BlogCard blog={post} />
-    </div>
-}
-
-)}
+                {posts.map((post, index) => (
+                    <div key={index}>
+                        {/* Blog card */}
+                        <BlogCard blog={post} />
+                    </div>
+                ))}
             </div>
         </div>
-    )
-}
+    );
+};
 
-export default Blog
+export default Blog;
